809398d3e409a9c3d2def4c997a4f4352502bae2
attonnnn <attonnnn@gmail.com>
2013-04-28 22:29:47 +0200

    datepicker fix

--- roomeeting-web/src/main/webapp/bootstrap/datepicker/bootstrap-datepicker.js
+++ roomeeting-web/src/main/webapp/bootstrap/datepicker/bootstrap-datepicker.js
@@ -557,7 +557,6 @@
 								}
 							}
 							this._setDate(UTCDate(year, month, day,0,0,0,0));
-							this.picker.hide();
 						}
 						break;
 				}
@@ -960,3 +959,14 @@
 	jQuery.fn.datepicker.DPGlobal = DPGlobal;
 
 }( window.jQuery );
+
+(function($){
+	$.fn.datepicker.dates['fr'] = {
+		days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
+		daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"],
+		daysMin: ["D", "L", "Ma", "Me", "J", "V", "S", "D"],
+		months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+		monthsShort: ["Jan", "Fev", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "Dec"],
+		today: "Aujourd'hui"
+	};
+}(window.jQuery));
--- roomeeting-web/src/main/webapp/js/roomeeting.js
+++ roomeeting-web/src/main/webapp/js/roomeeting.js
@@ -5,6 +5,11 @@ if(!RooMeeting)
 }
 
 jQuery(document).ready(function () {
-	jQuery('.datepicker-input').datepicker();
+	jQuery('.datepicker-input').datepicker({
+		autoclose : true,
+		format:"dd/mm/yyyy",
+		weekStart:"1",
+		language:'fr'
+	});
 	jQuery(".tooltiped").tooltip();
 });
