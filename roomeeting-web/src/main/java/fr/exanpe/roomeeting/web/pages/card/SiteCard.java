d8d3446c5dfaf7549b175a99ff040e9981bc9999
attonnnn <attonnnn@gmail.com>
2013-05-13 18:56:45 +0200

    Adding Cards + cleanup

--- roomeeting-web/src/main/java/fr/exanpe/roomeeting/web/pages/card/SiteCard.java
+++ roomeeting-web/src/main/java/fr/exanpe/roomeeting/web/pages/card/SiteCard.java
@@ -1,29 +1,30 @@
 package fr.exanpe.roomeeting.web.pages.card;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.tapestry5.PersistenceConstants;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 import fr.exanpe.roomeeting.domain.business.SiteManager;
-import fr.exanpe.roomeeting.domain.model.Room;
+import fr.exanpe.roomeeting.domain.model.Site;
 
-public class RoomCard
+public class SiteCard
 {
     @Inject
     private SiteManager siteManager;
 
     @Property
     @Persist(PersistenceConstants.FLASH)
-    private Room room;
+    private Site site;
 
     void onActivate(long id)
     {
-        room = siteManager.findRoom(id);
+        site = siteManager.find(id);
     }
 
     public boolean hasMap()
     {
-        return room.getMap() != null && room.getMap().length > 0;
+        return StringUtils.isNotEmpty(site.getLatitude()) && StringUtils.isNotEmpty(site.getLongitude());
     }
 }
