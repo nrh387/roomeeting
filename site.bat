d08003c45db29659a6e83f10fd30407a24d2fbcb
jmaupoux <jmaupoux@ITEM-57623.dhcp.manh.fr.sopra>
2013-06-29 23:05:15 +0200

    Moving site in a specific module

--- site.bat
+++ site.bat
@@ -1,1 +1,3 @@
-D:\tools\org\apache\maven\apache-maven-3.0.3\bin\mvn generate-sources site -N
+set MAVEN_OPTS=-Xmx1024m
+cd roomeeting-site/
+D:\tools\org\apache\maven\apache-maven-3.0.3\bin\mvn clean generate-sources site -X
